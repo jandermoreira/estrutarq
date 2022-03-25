0669926e825be59728bd66583f1c33e3d985f687
Jander <moreira.jander@gmail.com>
2022-03-25 11:00:01 -0300

    Auto sex 25 mar 2022 11:00:01 -03

--- arquivo/arquivo_comum.py
+++ arquivo/arquivo_comum.py
@@ -1,44 +1,85 @@
 #
-#  Blocos
+#  Arquivos
 #
 
-from typing import BinaryIO
-from os import fstat
+# from os import fstat
+from abc import ABCMeta, abstractmethod
+from os.path import exists
 
 
-# from dado import DadoFixo
-
-class GerenciadorArquivo:
+class ArquivoBasico(metaclass = ABCMeta):
     """
-    Gerenciador dedicado a um único arquivo aberto. O arquivo pode ser
-    simples ou usando blocos, cada um deles podendo conter registros de
-    comprimento fixo ou variável
+    Gerenciador dedicado a um único arquivo aberto
     """
 
-    def __init__(self, arquivo: BinaryIO, tipo: str):
+    def __init__(self, nome_arquivo: str, tipo: str, novo: bool = False):
         self.tipo = tipo
-        self.arquivo = arquivo
+        self.nome_arquivo = nome_arquivo
+        if not exists(self.nome_arquivo) or novo:
+            self._crie_arquivo_novo()
+        else:
+            self._abra_arquivo_existente()
+
+    def _crie_arquivo_novo(self):
+        """
+            Criação de um arquivo novo, com seu cabeçalho
+        """
+        try:
+            self._arquivo = open(self.nome_arquivo, "wb")
+        except IOError:
+            raise IOError(f"Erro de criação do arquivo {self.nome_arquivo}.")
+        else:
+            self.inicie_arquivo_novo()
+
+    def _abra_arquivo_existente(self):
+        """
+
+        """
+        try:
+            self._arquivo = open(self.nome_arquivo, "rb")
+        except IOError:
+            raise IOError(f"Erro de abertura do arquivo {self.nome_arquivo}.")
+        else:
+            self.inicie_arquivo_existente()
+
+    @abstractmethod
+    def inicie_arquivo_novo(self):
+        """
+        Iniciação necessária à criação de um novo arquivo
+        """
+        pass
+
+    @abstractmethod
+    def inicie_arquivo_existente(self):
+        """
+        Iniciação necessária para a abertura de um arquivo já existente
+        """
+        pass
 
 
-class GASimplesFixo(GerenciadorArquivo):
+class ArquivoSimplesFixo(ArquivoBasico):
     """
     Gerenciador de arquivo simples (como fluxo de dados) com registros de
     comprimento fixo.
     """
 
-    def __init__(self, arquivo: BinaryIO, comprimento_registro: int):
-        super().__init__(arquivo, "simples fixo")
+    def __init__(self, nome_arquivo: str, comprimento_registro: int):
+        super().__init__(nome_arquivo, "simples fixo")
         self.comprimento_registro = comprimento_registro
 
+    def inicie_arquivo_novo(self):
+        pass
 
-class GABloco:
+    def inicie_arquivo_existente(self):
+        pass
 
-    def __init__(self, arquivo: BinaryIO, comprimento_bloco: int):
-        self.arquivo = arquivo
-        self.comprimento_bloco = comprimento_bloco
-        self.proximo_novo = fstat(
-            self.arquivo.fileno()).st_size / comprimento_bloco
-
+# class GABloco:
+#
+#     def __init__(self, arquivo: BinaryIO, comprimento_bloco: int):
+#         self.arquivo = arquivo
+#         self.comprimento_bloco = comprimento_bloco
+#         self.proximo_novo = fstat(
+#             self.arquivo.fileno()).st_size / comprimento_bloco
 
 # class BlocoBasico:
 #     def __init__(self, comprimento_bloco: int):
