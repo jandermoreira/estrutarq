8dbbdf02e3c230805cd5df206b50b40c033897fd
Jander <moreira.jander@gmail.com>
2022-03-26 13:20:01 -0300

    Auto sáb 26 mar 2022 13:20:01 -03

--- testes/teste_arquivos.py
+++ testes/teste_arquivos.py
@@ -17,14 +17,14 @@ dados = [[nome, sobrenome, endereco]
 
 
 def main():
-    registro = RegistroTerminador(
+    registro = RegistroFixo(50,
         ("nome", CampoCadeiaTerminador()),
         ("sobrenome", CampoCadeiaPrefixado()),
         ("endereco", CampoCadeiaTerminador()),
     )
     arquivo = ArquivoSimplesFixo("/tmp/arq.dat", registro, novo = True)
 
-    numero_registros = 5
+    numero_registros = 30
     for nome, sobrenome, endereco in sample(dados, numero_registros):
         registro.nome.valor = nome
         registro.sobrenome.valor = sobrenome
@@ -32,12 +32,13 @@ def main():
         arquivo.escreva(registro)
     arquivo.feche()
 
+    # registro.adicione_campos(("rnn", CampoIntBinario()))
     arquivo = ArquivoSimplesFixo("/tmp/arq.dat", registro)
     fim_de_arquivo = False
     while not fim_de_arquivo:
         try:
             registro = arquivo.leia()
-        except IOError:
+        except EOFError:
             fim_de_arquivo = True
         else:
             print("-----------------")
