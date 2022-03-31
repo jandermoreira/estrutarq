c545894b0286dcbfa00940b6998f55558339a5eb
Jander <moreira.jander@gmail.com>
2022-03-31 09:40:01 -0300

    Auto qui 31 mar 2022 09:40:01 -03

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('/home/jander/graduacao/livros/estruturas_de_arquivos/codigo/estrutarq'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -43,7 +43,7 @@ language = 'pt_BR'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'testes']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
