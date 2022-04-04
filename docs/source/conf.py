748491ca2b8f7040ab9d1c6a3741e860500869d4
Jander <moreira.jander@gmail.com>
2022-04-04 12:00:01 -0300

    Auto seg 04 abr 2022 12:00:01 -03

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
-    'sphinx.ext.rst',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
