f4b0fbd7ebd0307b069705af7362d1f64f75b4d7
Jander <moreira.jander@gmail.com>
2022-07-01 13:33:46 -0300

    Implementação dos métodos para obter bits e bytes + testes

--- testes/teste_fluxo.py
+++ testes/teste_fluxo.py
@@ -9,17 +9,75 @@ from bitarray import bitarray
 from utilitarios.fluxo import Fluxo
 
 
+def apresente_bytes(sequencia: bytes):
+    """
+    Torna os bytes "bonitos"
+    :param sequencia: os bytes
+    """
+    return ":".join([f"{v:02X}" for v in sequencia])
+
+
 def main():
     fluxo = Fluxo()
-    print(0, fluxo)
 
-    for n in range(20):
-        fluxo.adicione_bits(bitarray(str(randint(0, 0))))
-        print(n + 1, fluxo, len(fluxo))
+    # TESTE
+    print("Teste: inserção bit a bit")
+    bits_teste = bitarray(20)
+    for bit in bits_teste.tolist():
+        fluxo.adicione_bits(bitarray(str(bit)))
+    print(bits_teste)
 
-    for n in range(20):
-        bit = fluxo.obtenha_bits(1)
-        print(bit, fluxo)
+    bits_recuperados = bitarray()
+    while len(fluxo) > 0:
+        bits_recuperados += fluxo.obtenha_bits(1)
+    print(bits_recuperados)
+
+    if bits_recuperados == bits_teste:
+        print("Ok")
+    else:
+        print("Erro")
+
+    # TESTE
+    print("Teste: inserção de sequências de comprimentos diferentes")
+    fluxo.limpe()
+    bits_teste = bitarray()
+    for n in range(10):
+        bits = bitarray(randint(1, 5))
+        fluxo.adicione_bits(bits)
+        bits_teste += bits
+    print(bits_teste)
+
+    bits_recuperados = bitarray()
+    while len(fluxo) > 0:
+        bits_recuperados += fluxo.obtenha_bits(randint(0, 4))
+    print(bits_recuperados)
+
+    if bits_recuperados == bits_teste:
+        print("Ok")
+    else:
+        print("Erro")
+
+    # TESTE
+    print("Teste: remoção de bytes")
+    fluxo.limpe()
+    bits_teste = bitarray(60)
+    fluxo.adicione_bits(bits_teste)
+    print(bits_teste, fluxo)
+    fluxo_teste = str(fluxo)[1:-2]
+
+    fim = False
+    bytes_recuperados = b""
+    while not fim:
+        byte = fluxo.obtenha_bytes(randint(1, 4))
+        print(apresente_bytes(byte), fluxo)
+        fim = len(byte) == 0
+        bytes_recuperados += byte
+
+    if fluxo_teste == apresente_bytes(bytes_recuperados) + str(fluxo)[1:-2]:
+        print("Ok")
+    else:
+        print("Erro")
+
 
 if __name__ == '__main__':
     main()
--- utilitarios/fluxo.py
+++ utilitarios/fluxo.py
@@ -9,7 +9,7 @@ Implementação de conversão de fluxo de bits para fluxo de bytes e vice-versa.
 from bitarray import bitarray
 
 
-class Fluxo():
+class Fluxo:
     """
     Classe para fluxo de bits e bytes, com armazenamento interno no formato de
     bytes e bitarrays.
@@ -18,9 +18,7 @@ class Fluxo():
     """
 
     def __init__(self):
-        self.fluxo_bytes = b""
-        self.bits_inicio = bitarray()
-        self.bits_final = bitarray()
+        self.fluxo = bitarray()
 
     def adicione_bits(self, sequencia: bitarray) -> None:
         """
@@ -29,22 +27,49 @@ class Fluxo():
 
         :param sequencia: sequencia de bits.
         """
-        self.bits_final += sequencia
-        # numero_bytes = len(self.bits_final) // 8
-        # self.fluxo_bytes += self.bits_final[:numero_bytes * 8].tobytes()
-        # self.bits_final = self.bits_final[numero_bytes * 8:]
+        self.fluxo += sequencia
 
-    def obtenha_bits(self, comprimento) -> bitarray:
+    def obtenha_bits(self, comprimento: int = None) -> bitarray:
         """
-        Recupera e retorna a quantidade de bits solicitada, usando os bits
-        em bit_inicio e, se necessário, os do fluxo de bytes. Não havendo
-        quantidade de bits suficiente, somente as contidas no fluxo são
-        retornadas (caso em que a quantidade é menor que a solicitada).
+        Recupera e retorna a quantidade de bits solicitada do fluxo de bits.
+        Se o comprimento não for especificado, o fluxo inteiro é retornado.
         Se o fluxo estiver vazio, o retorno também é um bitarray vazio.
 
         :param comprimento: número de bits
         :return: os bits solicitados
         """
+        if comprimento is None:
+            comprimento = len(self.fluxo)
+        bits = self.fluxo[:comprimento]
+        self.fluxo = self.fluxo[comprimento:]
+        return bits
+
+    def obtenha_bytes(self, comprimento: int = None) -> bytes:
+        """
+        Recupera a quantidade de bytes solicitada, retornada como class:`bytes`,
+        considerando apenas bytes "inteiros" (i.e., 8 bits). Os bits
+        remanescentes permanecem no fluxo. Se o comprimento não for
+        especificado, todos os bytes inteiros são retornados. Se o fluxo
+        estiver vazio, o retorno também é vazio.
+
+        :param comprimento, int: número de bytes
+        :return: os bytes solicitados
+        """
+        if len(self.fluxo) < 8:
+            sequencia_bytes = b""
+        else:
+            if comprimento is None:
+                comprimento = len(self.fluxo)//8
+            comprimento = min(comprimento, len(self.fluxo)//8)
+            sequencia_bytes = self.fluxo[:comprimento * 8].tobytes()
+            self.fluxo = self.fluxo[comprimento * 8:]
+        return sequencia_bytes
+
+    def limpe(self) -> None:
+        """
+        Limpa o fluxo de bits.
+        """
+        self.fluxo = bitarray()
 
     def __len__(self) -> int:
         """
@@ -52,21 +77,15 @@ class Fluxo():
 
         :return: a quantidade de bits no fluxo
         """
-        return len(self.bits_inicio) + 8 * len(self.fluxo_bytes) + \
-               len(self.bits_final)
+        return len(self.fluxo)
 
-    def __str__(self):
+    def __str__(self) -> str:
         """
         Apresenta o fluxo de bytes.
         """
-        # inicio = "".join([str(b) for b in self.bits_inicio.tolist()])
-        # if len(inicio) > 0:
-        #     inicio = inicio + "..."
-        # fluxo = ":".join(f"{b:02X}" for b in self.fluxo_bytes)
-        numero_bytes = len(self.bits_final) // 8
-        self.fluxo_bytes += self.bits_final[:numero_bytes * 8].tobytes()
-        final = "".join([str(b) for b in self.bits_final.tolist()])
-        if len(final) > 0:
-            final = "..." + final
-        # return f"[{inicio}{fluxo}{final}]"
-        return final
+        numero_bytes = len(self.fluxo) // 8
+        fluxo_bytes = self.fluxo[:numero_bytes * 8].tobytes()
+        em_bytes = ":".join([f"{byte:02X}" for byte in fluxo_bytes])
+        em_bits = "".join([str(b) for b
+                           in self.fluxo[numero_bytes * 8:].tolist()])
+        return f"[{em_bytes}.{em_bits}]"
