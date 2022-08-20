f50550e69a460fe3e793d0410787585f3223c12b
Jander <moreira.jander@gmail.com>
2022-08-20 16:38:16 -0300

    Compressão e descompressão com quantidade variável (crescente) de bits

--- compressao/lzw.py
+++ compressao/lzw.py
@@ -12,93 +12,173 @@ Cengage Learning, 2016
 
 # todo: considerar https://stackoverflow.com/questions/4730993/python-key-in-dict-keys-performance-for-large-dictionaries
 
+from typing import Union
+
+from bitarray import bitarray
+from bitarray.util import ba2int
+
 from estrutarq.utilitarios.fluxo import Fluxo
 
+comprimento_maximo_codigo = 10  # em bits
 
-class LZW:
+
+class LZWcompressor:
     """
+    Classe para compressão pelo método LZW.
 
+    O dicionário inicial é composto pelos 256 possíveis bytes.
     """
 
     def __init__(self):
-        self.lista_codigos = []
+        self.concatenacao_de_simbolos = None
+        self.fluxo = None
+        self.dicionario = None
+        self.comprimento_codigo = None
+        self.primeira_entrada = True
+        self.simbolo_corrente = None
 
-    def processe_de_bytes(self, sequencia: bytes):
+    def _int_to_bitarray(self, codigo: int) -> bitarray:
+        """
+        Converte um inteiro para um bitarray com o comprimento atual em bits
+        do código
+        :param codigo: código do dicionário
+        :return: a sequência de bits relativa ao código
+        :rtype: bitarray
+        """
+        bits = bitarray(f"{codigo:0{self.comprimento_codigo}b}")
+        # if len(bits) > comprimento_maximo_codigo:
+        #     raise ValueError
+        return bits
+
+    def _inicie_dicionario(self):
+        """
+        Inicia o dicionário de compressão com os 256 bytes iniciais
+        """
+        self.dicionario = dict([(bytes([x]), x) for x in range(256)])
+        self.comprimento_codigo = 9
+
+    def _atualize_dicionario_e_fluxo(self, codigo):
+        self.fluxo.adicione_bits(self._int_to_bitarray(codigo))
+
+        self.dicionario[self.concatenacao_de_simbolos +
+                        self.simbolo_corrente] = len(self.dicionario)
+        if len(self.dicionario) == 2 ** self.comprimento_codigo:
+            self.comprimento_codigo += 1
+            print(f"(*{self.comprimento_codigo})")
+
+        # print("Saída :",
+        #       f"{codigo:0{self.comprimento_codigo}b}",
+        #       self.dicionario[self.concatenacao_de_simbolos],
+        #       f"'{self.concatenacao_de_simbolos}'",
+        #       f"({len(self.dicionario)}) {self.comprimento_codigo} bits")
+
+    def processe_de_bytes(self, sequencia: Union[bytes, str]):
         """
         Realiza a compressão da sequência de bytes, considerando que cada
         byte é um símbolo do conjunto original.
 
-        :param sequencia: sequência de bytes
+        :param sequencia: sequência de dados
         """
-        dicionario = dict([(bytes([x]), x) for x in range(256)])
 
-        simbolo = bytes([sequencia[0]])
+        # Na primeira entrada de dados: criação do dicionário padrão e mais
+        # outras iniciações necessárias
+        if self.primeira_entrada:
+            self._inicie_dicionario()
+            self.fluxo = Fluxo()
+            self.concatenacao_de_simbolos = bytes([sequencia[0]])
 
-        comprimento_longo = 1
-        comprimento = 8
-        comprimento_codificacao = 0
-        self.lista_codigos = []
-        n = 1
-        for simbolo_corrente in sequencia[1:]:
-            simbolo_corrente = bytes([simbolo_corrente])
-            if simbolo + simbolo_corrente in dicionario:
-                simbolo += simbolo_corrente
-                if len(simbolo) > comprimento_longo:
-                    comprimento_longo = len(simbolo)
+        # Processamento da sequência de dados
+        posicao = 1
+        while posicao < len(sequencia):
+            self.simbolo_corrente = bytes([sequencia[posicao]])
+            if (self.concatenacao_de_simbolos +
+                self.simbolo_corrente) in self.dicionario:
+                self.concatenacao_de_simbolos += self.simbolo_corrente
             else:
-                self.lista_codigos.append(dicionario[simbolo])
-                print(
-                    f"[{simbolo.decode('latin'):30s}; {dicionario[simbolo]:>0{comprimento}b}] --- ",
-                    end = "")
-                print(f"{dicionario[simbolo]:7d}", ' ', end = '')
-                dicionario[simbolo + simbolo_corrente] = len(dicionario)
-                simbolo = simbolo_corrente
-                comprimento_codificacao += comprimento
-                print(f" ({n * 8}/{comprimento_codificacao}: " +
-                      f"{100 * (n * 8 - comprimento_codificacao) / (n * 8):.1f}%) ")
+                self._atualize_dicionario_e_fluxo(
+                    self.dicionario[self.concatenacao_de_simbolos])
+                self.concatenacao_de_simbolos = self.simbolo_corrente
+                # if self.comprimento_codigo > comprimento_maximo_codigo:
+                #     self._inicie_dicionario()
+                #     print(">", self.dicionario[self.simbolo_corrente], len(self.dicionario))
 
-            if len(dicionario) >= 2 ** comprimento:
-                comprimento += 1
-                # print(f"comprimento: {comprimento}")
-            n += 1
-            # if comprimento_codificacao > n * 8:
-            #     print(" <<<", len(dicionario))
-            # else:
-            #     print()
-            # if len(dicionario) > 2 ** 12:
-            #     self.lista_codigos.append(None)
-            #     dicionario = dict([(bytes([x]), x) for x in range(256)])
-            #     print("reset")
-        self.lista_codigos.append(dicionario[simbolo])
-        # print(dicionario[simbolo])
-        print(">>>", comprimento_longo)
-        print(f"{len(sequencia) * 8}; {comprimento_codificacao}; " +
-              f"{100 * (len(sequencia) * 8 - comprimento_codificacao) / len(sequencia) / 8:.1f}")
+            posicao += 1
 
-    def recupere_de_lista(self):
+        self.primeira_entrada = False
 
-        dicionario = dict([(x, bytes([x])) for x in range(256)])
+    def feche(self):
+        """
+        Encerra a entrada de dados e insere o última código no fluxo
+        :return:
+        """
+        self._atualize_dicionario_e_fluxo(
+            self.dicionario[self.concatenacao_de_simbolos])
 
-        codigo_base = self.lista_codigos[0]
-        # print(dicionario[codigo_base], end = "")
-        sequencia = dicionario[codigo_base]
-        comprimento = 8
-        for codigo_corrente in self.lista_codigos[1:]:
-            if codigo_corrente in dicionario:
-                dicionario[len(dicionario)] = dicionario[codigo_base] + \
-                                              bytes([dicionario[
-                                                         codigo_corrente][0]])
-                # print(dicionario[codigo_corrente], end = "")
-                sequencia += dicionario[codigo_corrente]
+
+class LZWdescompressor:
+    """
+    Classe para descompressão de um fluxo de bits pelo método LZW.
+    
+    O dicionário inicial é composto pelos 256 possíveis bytes.
+    """
+
+    def __init__(self):
+        self.fluxo = Fluxo()
+        self.primeira_entrada = True
+        self.sequencia = None
+        self.comprimento = None
+        self.dicionario = None
+        self._inicie_dicionario()
+
+    def _inicie_dicionario(self):
+        """
+        Inicia o dicionário de descompressão com os 256 bytes iniciais
+        """
+        self.dicionario = dict([(x, bytes([x])) for x in range(256)])
+        self.comprimento = 9
+
+    def recupere_de_fluxo(self, fluxo: Union[bitarray, Fluxo]):
+        if isinstance(fluxo, Fluxo):
+            fluxo_bits = fluxo
+        else:
+            fluxo_bits = Fluxo()
+            fluxo_bits.adicione_bits(fluxo)
+        bits = fluxo_bits.obtenha_bits(self.comprimento)
+        codigo_base = ba2int(bits)
+
+        self.sequencia = self.dicionario[codigo_base]
+        fim_de_fluxo = False
+        while not fim_de_fluxo:
+            bits = fluxo_bits.obtenha_bits(self.comprimento)
+            if len(bits) == 0:
+                fim_de_fluxo = True
             else:
-                dicionario[len(dicionario)] = dicionario[codigo_base] + \
-                                              bytes(
-                                                  [dicionario[codigo_base][0]])
-                # print(dicionario[codigo_base] + bytes([dicionario[codigo_corrente][0]]),
-                #       end = "")
-                sequencia += dicionario[codigo_base] + \
-                             bytes([dicionario[codigo_corrente][0]])
-            codigo_base = codigo_corrente
-            if codigo_corrente is None:
-                dicionario = dict([(x, bytes([x])) for x in range(256)])
-        return sequencia
+                codigo_corrente = ba2int(bits)
+                print(f"--<{codigo_base} {codigo_corrente}>")
+                print(f"[{codigo_corrente}] {len(bits)} ", end = "")
+                if codigo_corrente in self.dicionario:
+                    self.dicionario[len(self.dicionario)] = (
+                            self.dicionario[codigo_base] +
+                            bytes([self.dicionario[codigo_corrente][0]]))
+                    # print(f"({self.dicionario[codigo_corrente]})")
+                    self.sequencia += self.dicionario[codigo_corrente]
+                else:
+                    self.dicionario[len(self.dicionario)] = (
+                            self.dicionario[codigo_base] +
+                            bytes([self.dicionario[codigo_base][0]]))
+                    # print(
+                    #     f"{self.dicionario[codigo_base] + bytes([self.dicionario[codigo_corrente][0]])}",
+                    #     self.comprimento)
+                    self.sequencia += (
+                            self.dicionario[codigo_base] +
+                            bytes([self.dicionario[codigo_corrente][0]]))
+                codigo_base = codigo_corrente
+                # print(f"[{codigo_base}]", end = "")
+                # print(f"({self.dicionario[codigo_base]})")
+
+            if len(self.dicionario) == 2 ** self.comprimento - 1:
+                self.comprimento += 1
+                print(f"(*{self.comprimento})")
+
+
+        return self.sequencia
--- testes/teste_lzw.py
+++ testes/teste_lzw.py
@@ -4,28 +4,40 @@ Teste da compressão LZW
 """
 
 from sys import argv
+from os import system
 
-from estrutarq.compressao import LZW
+from estrutarq.compressao import LZWcompressor, LZWdescompressor
 
 if len(argv) > 1:
     arquivo = argv[1]
 else:
     arquivo = "gone_with_the_wind.txt"
 
-compressor = LZW()
-
-# Compressão
 with open(arquivo, "rb") as f:
-    texto_original = f.read()
-compressor.processe_de_bytes(texto_original)
+    dados = f.read(1393)
+comprimento_original = 8 * len(dados)
+
+print(f"Comprimindo {comprimento_original} bits...", end = "")
+compressor = LZWcompressor()
+compressor.processe_de_bytes(dados)
+compressor.feche()
+comprimento_comprimido = len(compressor.fluxo)
+print(" Terminado")
 
 # Descompressão
-texto_recuperado = compressor.recupere_de_lista()
+print(f"Descomprimindo {comprimento_comprimido} bits...", end = "",
+      flush = True)
+descompressor = LZWdescompressor()
+texto_recuperado = descompressor.recupere_de_fluxo(compressor.fluxo)
+print(" Terminado")
 
-print(texto_original == texto_recuperado)
+print("\nDeu certo?", "sim" if dados == texto_recuperado else "não")
+print(f"Original:   {comprimento_original:10d} bits")
+print(f"Comprimido: {comprimento_comprimido:10d} bits")
+taxa_compressao = (comprimento_original - comprimento_comprimido) / \
+                  comprimento_original * 100
+print(f"Taxa de compressão: {taxa_compressao:.3f}%")
 
-# with open("/tmp/saida", "wb") as f:
-#     f.write(texto_recuperado)
-# 
-# 
-# system(f"diff {arquivo} /tmp/saida && echo Ok")
+with open("/tmp/saida", "wb") as f:
+    f.write(texto_recuperado)
+system(f"diff {arquivo} /tmp/saida > /dev/null && echo Ok")
